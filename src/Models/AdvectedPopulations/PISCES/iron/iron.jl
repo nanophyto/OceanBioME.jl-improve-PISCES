c0747dd2e22e6d67a9465c431db4054c0721aa58
nanophyto <coralscaper@gmail.com>
2026-03-10 20:14:53 +0000

    Add IronInputs for dissolved iron kernel

--- src/Models/AdvectedPopulations/PISCES/iron/iron.jl
+++ src/Models/AdvectedPopulations/PISCES/iron/iron.jl
@@ -1,6 +1,6 @@
 module Iron
 
-export SimpleIron
+export SimpleIron, IronInputs
 
 using Oceananigans.Units
 
@@ -22,12 +22,29 @@ import OceanBioME.Models.PISCESModel: free_iron
 
 include("simple_iron.jl")
 
+"""Local scalar inputs for the dissolved iron tendency."""
+struct IronInputs{FT}
+    Fe::FT
+    DOC::FT
+    T::FT
+    scavenging_rate::FT
+    small_particle_iron_remineralisation::FT
+    phytoplankton_iron_uptake::FT
+    grazing_waste::FT
+    upper_trophic_waste::FT
+    colloidal_aggregation::FT
+    scavenging::FT
+    bacterial_uptake::FT
+end
+
 @inline function ligand_concentration(iron::SimpleIron, DOC)
     Lₜᵐᵃˣ = iron.maximum_ligand_concentration
     Lₜ = iron.dissolved_ligand_ratio * DOC - Lₜᵐᵃˣ
 
     return max(Lₜᵐᵃˣ, Lₜ)
 end
+
+@inline ligand_concentration(iron::SimpleIron, inputs::IronInputs) = ligand_concentration(iron, inputs.DOC)
 
 @inline function free_iron(::SimpleIron, Fe, DOC, T)
     # maybe some of these numbers should be parameters
@@ -38,26 +55,66 @@ end
     return (-Δ + √(Δ^2 + 4K * Fe)) / 2K
 end
 
+@inline free_iron(iron::SimpleIron, inputs::IronInputs) = free_iron(iron, inputs.Fe, inputs.DOC, inputs.T)
+
 @inline function free_iron(iron::SimpleIron, i, j, k, grid, bgc, clock, fields, auxiliary_fields)
     DOC = @inbounds fields.DOC[i, j, k]
-    Fe  = @inbounds fields.Fe[i, j, k]
-    T   = @inbounds fields.T[i, j, k]
+    Fe = @inbounds fields.Fe[i, j, k]
+    T = @inbounds fields.T[i, j, k]
 
     return free_iron(iron, Fe, DOC, T)
 end
 
-@inline function ligand_aggregation(iron::SimpleIron, scavenging_rate, Fe, total_ligand_concentration, free_iron_concentration)
+@inline function ligand_aggregation(
+    iron::SimpleIron,
+    scavenging_rate,
+    Fe,
+    total_ligand_concentration,
+    free_iron_concentration,
+)
     λ̄ = iron.excess_scavenging_enhancement
     excess_iron = max(zero(Fe), Fe - total_ligand_concentration)
 
     return λ̄ * scavenging_rate * excess_iron * free_iron_concentration
 end
 
-@inline function iron_tendency(small_particles, grazing_waste, upper_trophic_waste,
-                               consumption, ligand_aggregation_loss, colloidal_aggregation,
-                               scavenging, bacterial_uptake)
-    return (small_particles + grazing_waste + upper_trophic_waste
-            - consumption - ligand_aggregation_loss - colloidal_aggregation - scavenging - bacterial_uptake)
+@inline function ligand_aggregation(iron::SimpleIron, inputs::IronInputs)
+    return ligand_aggregation(
+        iron,
+        inputs.scavenging_rate,
+        inputs.Fe,
+        ligand_concentration(iron, inputs),
+        free_iron(iron, inputs),
+    )
+end
+
+@inline function iron_tendency(
+    small_particles,
+    grazing_waste,
+    upper_trophic_waste,
+    consumption,
+    ligand_aggregation_loss,
+    colloidal_aggregation,
+    scavenging,
+    bacterial_uptake,
+)
+    return (small_particles + grazing_waste + upper_trophic_waste - consumption -
+            ligand_aggregation_loss - colloidal_aggregation - scavenging - bacterial_uptake)
+end
+
+@inline function iron_tendency(iron::SimpleIron, inputs::IronInputs)
+    ligand_aggregation_loss = ligand_aggregation(iron, inputs)
+
+    return iron_tendency(
+        inputs.small_particle_iron_remineralisation,
+        inputs.grazing_waste,
+        inputs.upper_trophic_waste,
+        inputs.phytoplankton_iron_uptake,
+        ligand_aggregation_loss,
+        inputs.colloidal_aggregation,
+        inputs.scavenging,
+        inputs.bacterial_uptake,
+    )
 end
 
 end # module
